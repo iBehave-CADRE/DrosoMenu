9c4a32b06ba7aa94e1bb4e1b95ec2f369836c7d8
TimLeffke <tim.leffke@freenet.de>
2025-06-05 12:06:41 +0200

    Added python example files

---
+++ python/move.py
@@ -0,0 +1,34 @@
+from time import sleep
+import serial
+
+# Specify the COM port your device is connected to here
+COM_PORT = 'COM6'
+
+def move(ser, pos):
+    responses = ['Out', '1', '2', '3']
+    print(f"Moving to Position {responses[pos]}")
+
+    # Send position to the device
+    ser.write(f'{pos}'.encode('UTF-8'))
+
+    # Get and decode response of device
+    response = ser.readline().decode().strip()
+
+    # Check the response
+    if response != f'Moving to Position {responses[pos]}':
+        raise Exception("Device did not respond as expected!")
+
+    # Flush any additional
+    ser.flush()
+
+
+with serial.Serial(COM_PORT, timeout=5) as ser:
+    for pos in range(4):
+        # Move to position
+        move(ser, pos)
+
+        # Wait for a while
+        sleep(10)
+
+    # Return to 0 position
+    move(ser, 0)
