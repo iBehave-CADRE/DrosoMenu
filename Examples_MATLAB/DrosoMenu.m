ab8b4c08ee681cd02bc5d818b299b292672283ea
Benjamin Escribano <67735766+BenjaminEscribano@users.noreply.github.com>
2025-09-05 14:14:19 +0200

    Code Annotation

--- Examples_MATLAB/DrosoMenu.m
+++ Examples_MATLAB/DrosoMenu.m
@@ -1,10 +1,27 @@
-%%Establish a connection with the DrosoMenu
+
+
+
+
+
+
+
+%% Using the function serialport('COM', Baudrate) allows to create an object/variable that provides access to the DrosoMenu via serial communication.
+%% The Baudrate must be set to 9600, as this is the Baudrate defined in the Arduino code of the DrosoMenu controller.
+%% The COM port must be adapted to the one used by the DrosoMenu controller. This can be checked in the Arduino IDE under Tools -> Port.
 DrosoMenu = serialport("COM5", 9600);
+
+%%To correctly interpret the serial communincation content, a terminator must be defined. In the Arduino code of the DrosoMenu controller, a carriage return (CR) is used as terminator.
+%% Thus, the terminator of the serialport object must be set to "CR" as follows:
 configureTerminator(DrosoMenu,"CR");
 
-%% Move to position 0/out
+%% The function writeline(serialport, 'command') can be used to send commands to the DrosoMenu controller.
+%% The commands '0', '1', '2' and '3' will move the DrosoMenu to the corresponding position.
 writeline(DrosoMenu, '0');
+
+%% The function readline(serialport) can be used to read the response of the DrosoMenu controller.
 readout=readline(DrosoMenu);
+
+%% The function flush(serialport) can be used to clear the input and output buffer of the serialport object.
 flush(DrosoMenu);
 
 %% Move to position 1
@@ -22,5 +39,5 @@ writeline(DrosoMenu, '3');
 readout=readline(DrosoMenu);
 flush(DrosoMenu);
 
-%%
+%% The DrosoMenu can be disconnected by deleting the created object via serialport:
 delete(DrosoMenu);
--- Examples_MATLAB/README.md
+++ Examples_MATLAB/README.md
@@ -1,3 +1,32 @@
 # Example Code for MATLAB Implementation
 
-This code illustrated how to connect to the DrosoMenu in MATLAB via serial communication. After a successul connection is established, sending a 0, 1, 2 or 3 will move the DrosoMenu to the corresponding position.
+This code illustrated how to connect to the DrosoMenu in MATLAB via serial communication. After a successul connection is established, sending a 0, 1, 2 or 3 will move the DrosoMenu to the corresponding position:
+
+Use the serialport('COM', Baudrate) to connect to the Arduino in the DrosoMenu controller.
+
+To correctly interpret the serial communincation content, a terminator must be defined. 
+
+configureTerminator(serialport, "CR)
+
+Will send the 
+
+writeline(serialport, '0')
+
+
+%%Establish a connection with the DrosoMenu
+DrosoMenu = serialport("COM5", 9600);
+configureTerminator(DrosoMenu,"CR");
+
+%% Move to position 0/out
+writeline(DrosoMenu, '0');
+readout=readline(DrosoMenu);
+flush(DrosoMenu);
+
+
+The DrosoMenu can be disconnected by deleting the created object via serialport:
+
+delete(DrosoMenu);
+
+
+
+
