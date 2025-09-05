3170890aa9e2c0293b1da985237ff68bd08bced2
Benjamin Escribano <67735766+BenjaminEscribano@users.noreply.github.com>
2025-09-05 10:32:56 +0200

    Update of Manufacturing Files and Matlab Code

---
+++ Examples_MATLAB/DrosoMenu.m
@@ -0,0 +1,25 @@
+%%Establish a connection with the DrosoMenu
+DrosoMenu = serialport("COM5", 9600);
+configureTerminator(DrosoMenu,"CR");
+
+%% Move to position 0/out
+writeline(DrosoMenu, '0');
+readout=readline(DrosoMenu);
+flush(DrosoMenu);
+%% Move to position 1
+writeline(DrosoMenu, '1');
+readout=readline(DrosoMenu);
+flush(DrosoMenu);
+
+%% Move to position 2
+writeline(DrosoMenu, '2');
+readout=readline(DrosoMenu);
+flush(DrosoMenu);
+
+%% Move to position 3
+writeline(DrosoMenu, '3');
+readout=readline(DrosoMenu);
+flush(DrosoMenu);
+
+%%
+delete(DrosoMenu);
